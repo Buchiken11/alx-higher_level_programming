a9322ed4267e0507342e6637d023467f6298e1d6
Buchiken11 <buchiken025@gmail.com>
2023-12-28 02:23:45 +0100

    task 3-6

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,4 @@
+def only_diff_elements(set_1, set_2):
+    only_diff_set = set_1 ^ set_2  # Use the symmetric difference operator
+
+    return only_diff_set  # Return the resulting set
---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    # Sort the keys
+    sorted_keys = sorted(a_dictionary.keys())
+
+    # Print the sorted dictionary
+    for key in sorted_keys:
+        value = a_dictionary[key]
+        return value
