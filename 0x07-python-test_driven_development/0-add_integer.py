6000ab873e7d4879180b8ac21013ef2d0f4d3729
Buchiken11 <buchiken025@gmail.com>
2024-01-07 22:24:53 +0100

    task 1-7

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,28 +1,30 @@
 #!/usr/bin/python3
+"""
+Addition Module
+"""
 
-    '''
-    Addition of two arguments Module
-    '''
-    def add_integer(a, b=98):
-        '''
-        this fuction adds two arguments together either int or float
-        Args:
-            a = integer or floats
-            b = integer or floats
 
-        Raises:
-            TypeError: if not integer or Float
+def add_integer(a, b=98):
+    """
+    Function to add two integers or floats
 
-        Returns:
-            sum of the integers or if float, casted to int
-        '''
-        if (not isinstance(a, int) and not isinstance(a, float)):
-            raise TypeError("a must be an integer")
-        if not isinstance(b, int and not isinstance(b, float))
-            raise TypeError("b must be an integer")
-        if isinstance(a, float):
-            a = int(a)
-        if isinstance(b, float):
-            b = int(b)
+    Args:
+        a (number): Float or Number
+        b (:obj:number:optional): Optional Float or Integer
 
-        return a + b
+    Raises:
+        TypeError: If not float or integer
+
+    Returns:
+        float:int: Integer or float
+    """
+
+    if (not isinstance(a, int) and not isinstance(a, float)):
+        raise TypeError("a must be an integer")
+    if (not isinstance(b, int) and not isinstance(b, float)):
+        raise TypeError("b must be an integer")
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+    return a + b
