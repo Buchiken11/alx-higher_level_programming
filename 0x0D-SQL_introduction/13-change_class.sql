64e3281b326768f8064647831eb1c185dc652347
Buchiken11 <buchiken025@gmail.com>
2024-02-17 16:50:46 +0100

    task

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,6 +1,7 @@
 --  a script that removes all records with a score <= 5 in the table second_table
 
-DROP TABLE `second_table`
+
+DELETE FROM `second_table`
 FROM `score`, `name`
 WHERE `score` <= 5
 ORDER BY `score`;
