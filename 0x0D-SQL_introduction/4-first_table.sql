e5b5cab90d914c63038bf25b37c74f8ffbab402f
Buchiken11 <buchiken025@gmail.com>
2024-02-17 12:07:36 +0100

    task

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,4 +1,4 @@
 -- Write a script that creates a table called first_table in the current database in your MySQL server.
 
-CREATE TABLE IF NOT EXISTS first_table(
-	VARCHAR(256), INT)
+CREATE TABLE IF NOT EXISTS `first_table`(
+	`name` VARCHAR(256), `id` INT)
