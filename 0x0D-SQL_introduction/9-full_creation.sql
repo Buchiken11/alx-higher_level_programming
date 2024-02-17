37d8dd6b61fe2ca0074c7aa8ce41de66e1fbf723
Buchiken11 <buchiken025@gmail.com>
2024-02-17 15:51:19 +0100

    task5

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -5,4 +5,4 @@ CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score
 INSERT INTO TABLE `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
 INSERT INTO TABLE  `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
 INSERT INTO TABLE `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
-INSERT INTO TABLE `second_rable` (`id`, `name`, `score`) VALUES (4, "George", 8);
+INSERT INTO TABLE `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
