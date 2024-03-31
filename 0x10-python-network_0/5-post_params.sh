eba51c2c6122de6184154dd1c14bd6e7d1ca2072
Buchiken11 <buchiken025@gmail.com>
2024-03-31 06:57:52 +0100

    6-peak.py

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,9 +1,4 @@
-#!/bin/bash
-#Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-#
-url="$1"
-email="test@gmail.com"
-subject="I will always be here for PLD"
 
-# Send a POST request with the variables and display the response body
-curl -sL -X POST -d "email=$email&subject=$subject" "$url"
+#!/bin/bash
+#POST request to the passed URL using curl, and display the body of the response
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
